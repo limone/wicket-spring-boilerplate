48adaeca12349abcd9e1151e4774362c8ca13ff6
Michael Laccetti <michael@laccetti.com>
2012-03-01 13:52:55 -0500

    * Removing unnecessary files * Updated the HomePage to use code suggested by @martin-g on the Wicket mailing list

--- src/main/resources/archetype-resources/src/main/java/HomePage.java
+++ src/main/resources/archetype-resources/src/main/java/HomePage.java
@@ -1,9 +1,9 @@
 package ${package};
 
 import org.apache.wicket.AttributeModifier;
+import org.apache.wicket.markup.html.IHeaderResponse;
 import org.apache.wicket.markup.html.WebComponent;
 import org.apache.wicket.markup.html.WebPage;
-import org.apache.wicket.markup.html.internal.HtmlHeaderContainer;
 import org.apache.wicket.model.Model;
 import org.apache.wicket.request.cycle.RequestCycle;
 import org.apache.wicket.util.string.UrlUtils;
@@ -20,17 +20,14 @@ public class HomePage extends WebPage {
     add(new WebComponent("ie9js").add(new AttributeModifier("src", new Model<>(UrlUtils.rewriteToContextRelative("js/ie9.js", RequestCycle.get())))));
   }
   
-
   @Override
-  public void renderHead(HtmlHeaderContainer container) {
-    super.renderHead(container);
+  public void renderHead(IHeaderResponse response) {
+    response.renderJavaScriptReference("js/jquery-1.7.1.min.js");
+    response.renderJavaScriptReference("js/jquery-ui-1.8.17.custom.min.js");
+    response.renderJavaScriptReference("js/plugins.js");
+    response.renderJavaScriptReference("js/scripts.js");
     
-    container.getHeaderResponse().renderJavaScriptReference("js/jquery-1.7.1.min.js");
-    container.getHeaderResponse().renderJavaScriptReference("js/jquery-ui-1.8.18.min.js");
-    container.getHeaderResponse().renderJavaScriptReference("js/plugins.js");
-    container.getHeaderResponse().renderJavaScriptReference("js/scripts.js");
-    
-    container.getHeaderResponse().renderCSSReference("css/jquery-ui-1.8.18.css");
-    container.getHeaderResponse().renderCSSReference("css/style.css");
+    response.renderCSSReference("css/jquery-ui-1.8.17.custom.css");
+    response.renderCSSReference("css/style.css");
   }
 }
