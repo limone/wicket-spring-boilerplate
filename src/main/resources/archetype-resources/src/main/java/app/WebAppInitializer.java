e1e7ec262cd6fb58d29f727ad85780bb85dff0ca
Michael Laccetti <michael@laccetti.com>
2012-04-14 12:49:11 -0400

    * Turns out Spring was doubly initialized, and doubly searching, due to ctx.scan() in the WebAppInitializer along with the SpringConfiguration, so fixed that

--- src/main/resources/archetype-resources/src/main/java/app/WebAppInitializer.java
+++ src/main/resources/archetype-resources/src/main/java/app/WebAppInitializer.java
@@ -9,7 +9,7 @@ import org.slf4j.bridge.SLF4JBridgeHandler;
 import org.springframework.orm.jpa.support.OpenEntityManagerInViewFilter;
 import org.springframework.web.WebApplicationInitializer;
 import org.springframework.web.context.ContextCleanupListener;
-import org.springframework.web.context.ContextLoaderListener;
+import org.springframework.web.context.WebApplicationContext;
 import org.springframework.web.context.support.AnnotationConfigWebApplicationContext;
 
 public class WebAppInitializer implements WebApplicationInitializer {
@@ -24,17 +24,18 @@ public class WebAppInitializer implements WebApplicationInitializer {
     // Create the 'root' Spring application context
     AnnotationConfigWebApplicationContext ctx = new AnnotationConfigWebApplicationContext();
     ctx.register(SpringConfiguration.class);
-    ctx.scan("${package}");
     ctx.refresh();
+    
+    // Register the Spring Context in the ServletContext
+    sc.setAttribute(WebApplicationContext.ROOT_WEB_APPLICATION_CONTEXT_ATTRIBUTE, ctx);
 
     // Manages the lifecycle
-    sc.addListener(new ContextLoaderListener(ctx));
     sc.addListener(new ContextCleanupListener());
     
     // Filters
-    sc.addFilter("OpenEntityInViewFilter", OpenEntityManagerInViewFilter.class).addMappingForUrlPatterns(null, true, "/*");
+    sc.addFilter("OpenEntityInViewFilter", OpenEntityManagerInViewFilter.class).addMappingForUrlPatterns(null, true, "/*"); // Used for JPA, to ensure there's an EntityManager available
     
 
-    log.debug("${package} initialized.");
+    log.debug("${artifactId} initialized.");
   }
 }
